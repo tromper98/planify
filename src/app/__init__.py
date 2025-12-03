c23ca81b7fb751dc0e96b0bacd594d7e195893cf
Daniil Kolov <koloff.daniil@yandex.ru>
2025-12-03 14:54:33 +0300

    PL-7 Добавил telegram-api-bot и реализовал обработку базовую обработку входящего сообщения

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,1 +1,1 @@
-from .run import *
+from .tg_handler import *
--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
-from app import run
+from app import tg_handler
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+from infrastructure.envconfig import EnvConfig
+
 
 if __name__ == '__main__':
-    run()
+    application = ApplicationBuilder().token(EnvConfig.get_str('TG_BOT_TOKEN')).build()
+
+    start_handler = CommandHandler('start', tg_handler.start)
+
+    application.add_handler(start_handler)
+
+    application.run_polling()
